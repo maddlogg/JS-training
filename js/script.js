c198f6babab57e06fd382da36408c38b2ec06889
maddlogg <marshalmatthers97@gmail.com>
2024-08-18 13:38:33 +0300

    First task with JS film databasel

--- js/script.js
+++ js/script.js
@@ -1,18 +1,19 @@
-"use strict";
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
-let number = 5;
-const leftBorderWidth = 1;
-
-number = 10;
-console.log(number);
-
-const obj = {
-  a: 50,
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
 };
 
-obj.a = 10;
+const a = prompt("Один из последних просмотренных фильмов", ""),
+  b = +prompt("На сколько оцените его", ""),
+  c = prompt("Один из последних просмотренных фильмов", ""),
+  d = +prompt("На сколько оцените его", "");
 
-console.log(obj);
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-console.log(name);
-var name = "Ivan";
+console.log(personalMovieDB);
