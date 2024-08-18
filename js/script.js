24c197bbd171a6b3462dccc46f6ac165f55dc84d
maddlogg <marshalmatthers97@gmail.com>
2024-08-18 14:58:52 +0300

    Second task with cycles for and while

--- js/script.js
+++ js/script.js
@@ -8,12 +8,34 @@ const personalMovieDB = {
   privat: false,
 };
 
-const a = prompt("Один из последних просмотренных фильмов", ""),
-  b = +prompt("На сколько оцените его", ""),
-  c = prompt("Один из последних просмотренных фильмов", ""),
-  d = +prompt("На сколько оцените его", "");
+for (let i = 0; i < 2; i++) {
+  let lastMovie = prompt("Один из последних просмотренных фильмов", ""),
+    movieScore = prompt("На сколько оцените его", "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+  if (
+    lastMovie != null &&
+    movieScore != null &&
+    lastMovie != "" &&
+    movieScore != "" &&
+    lastMovie.length < 50
+  ) {
+    personalMovieDB.movies[lastMovie] = movieScore;
+    console.log("done");
+  } else {
+    console.log("error");
+    i--;
+  }
+}
+
+if (personalMovieDB.count < 10) {
+  console.log("Просмотрено довольно мало фильмов");
+  10;
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  console.log("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+  console.log("Вы киноман");
+} else {
+  console.log("Произошла ошибка");
+}
 
 console.log(personalMovieDB);
